a66e85e7dea0edb481bcc8b442ddb82c285d36a4
apd <apdcode@users.noreply.github.com>
2016-04-14 15:37:21 +0200

    0103 The Power of NumPy

---
+++ udacity/MLforTrading/scripts/0103 - NumPy.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 09:53:16 2016
+@author: apdcode
+"""
+
+'''0103    - The Power of NumPy
+
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+'''0103_06 - Creating NumPy arrays'''
