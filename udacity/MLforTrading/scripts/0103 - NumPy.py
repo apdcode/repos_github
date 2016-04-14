364f2160362a4008072e54304f025b2b8bc78d44
apd <apdcode@users.noreply.github.com>
2016-04-14 15:42:49 +0200

    0103 NumPy

--- udacity/MLforTrading/scripts/0103 - NumPy.py
+++ udacity/MLforTrading/scripts/0103 - NumPy.py
@@ -4,10 +4,29 @@ Created on Wed Apr 13 09:53:16 2016
 @author: apdcode
 """
 
-'''0103    - The Power of NumPy
+'''0103    - The Power of NumPy'''
 
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import numpy as np
+
+os.chdir('C:/repos_github/udacity/MLforTrading/')
+#os.chdir('C:/repos/research/data_misc')
+os.listdir(os.getcwd())
+
+def test_run():
+    # List to 1D array
+    print(np.array([2,3,4]))
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    test_run()
+
 
 '''0103_06 - Creating NumPy arrays'''
